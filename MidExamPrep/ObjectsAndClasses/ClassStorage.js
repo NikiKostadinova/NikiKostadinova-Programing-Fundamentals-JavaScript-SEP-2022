57ef04fb28c20f9acdd52c3dc40672a3dbf5eb75
NikiKostadinova <niki_radost@yahoo.com>
2024-01-24 19:19:50 +0200

    classStorage

--- MidExamPrep/ObjectsAndClasses/ClassStorage.js
+++ MidExamPrep/ObjectsAndClasses/ClassStorage.js
@@ -1,1 +1,39 @@
+function solve() {
 
+    class Storage {
+        constructor(capacity) {
+            this.capacity = capacity;
+            this.storage = new Array();
+            this.totalCost = 0;
+        }
+
+        addProduct(product) {
+            this.storage.push(product);
+            this.capacity -= product.quantity
+            this.totalCost += (product.price * product.quantity);
+        }
+        getProducts() {
+            let result = []
+            this.storage.map(item => {
+                result.push(JSON.stringify(item))
+            })
+            return result.join("\n");
+        }
+
+    }
+
+
+    let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
+    let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
+    let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
+    let storage = new Storage(50);
+    storage.addProduct(productOne);
+    storage.addProduct(productTwo);
+    storage.addProduct(productThree);
+    console.log(storage.getProducts());
+    console.log("capacity", storage.capacity);
+    console.log(storage.totalCost);
+
+}
+
+solve();
